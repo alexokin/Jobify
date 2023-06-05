3c47deb7c88a580c78367383caf08f524d07d16b
alexokin <alex.okin@gmail.com>
2023-06-05 11:20:58 +0300

    successful connection to mongoDB

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
 
-const app = express()
-dotenv.config()
+const app = express();
+dotenv.config();
+
+import connectDB from "./db/connect.js";
 
 // middleware
-import notFoundMiddleware from './middleware/not-found.js'
-import errorHandlerMiddleware from './middleware/error-handler.js'
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 
-app.get('/', (req,res) =>{
-    throw new Error('error')
-    res.send('Welcome!')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
 
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => { console.log(`Server is running on port ${port}...`); })
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
