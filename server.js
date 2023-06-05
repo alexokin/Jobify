c3fc97e5bbafef809d587ea2a8148ec0d3fe20e0
alexokin <alex.okin@gmail.com>
2023-06-05 10:48:12 +0300

    added error handling and not found middleware

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
 const app = express()
+dotenv.config()
+
+// middleware
+import notFoundMiddleware from './middleware/not-found.js'
+import errorHandlerMiddleware from './middleware/error-handler.js'
 
 app.get('/', (req,res) =>{
+    throw new Error('error')
     res.send('Welcome!')
 })
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
 
