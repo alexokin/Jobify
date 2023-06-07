ee9d7fb977806bf666225d2ae27173bdcd0732cb
alexokin <alex.okin@gmail.com>
2023-06-07 11:56:42 +0300

    added checking for email duplicate

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -1,8 +1,19 @@
+import { BadRequestError } from "../../errors/index.js";
 import User from "../../models/User.js";
 import { StatusCodes } from "http-status-codes";
 
 const register = async (req, res) => {
-  const user = await User.create(req.body);
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("Please provide all values");
+  }
+  const userExists = await User.findOne({email})
+  if (userExists) {
+    throw new BadRequestError('Email already in use.')
+  }
+
+  const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
