a831be44e5d675d5cc4552c4c430f3150cdd3f88
alexokin <alex.okin@gmail.com>
2023-06-07 15:44:16 +0300

    jwt and remove password from response

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -8,13 +8,25 @@ const register = async (req, res) => {
   if (!name || !email || !password) {
     throw new BadRequestError("Please provide all values");
   }
-  const userExists = await User.findOne({email})
+  const userExists = await User.findOne({ email });
   if (userExists) {
-    throw new BadRequestError('Email already in use.')
+    throw new BadRequestError("Email already in use.");
   }
 
   const user = await User.create({ name, email, password });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res
+    .status(StatusCodes.CREATED)
+    .json({
+      user: {
+        email: user.email,
+        lastName: user.lastName,
+        location: user.location,
+        name: user.name,
+      },
+      token,
+      location: user.location,
+    });
 };
 
 const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -15,7 +16,7 @@ const UserSchema = new mongoose.Schema({
     required: [true, "Please provide email"],
     validate: {
       validator: validator.isEmail,
-      message:'Please provide a valid email'
+      message: "Please provide a valid email",
     },
     unique: true,
   },
@@ -23,6 +24,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide password"],
     minlength: 6,
+    select: false,
   },
   lastName: {
     type: String,
@@ -38,10 +40,15 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-UserSchema.pre('save', async function () {
-  const salt = await bcrypt.genSalt(10)
-  this.password = await bcrypt.hash(this.password, salt)
-  
-})
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
 
 export default mongoose.model("User", UserSchema);
