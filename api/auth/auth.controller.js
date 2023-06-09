3d00936755b6431779262d40a3501d398b63c35a
alexokin <alex.okin@gmail.com>
2023-06-09 10:17:09 +0300

    adding login controller on the backend

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -1,4 +1,4 @@
-import { BadRequestError } from "../../errors/index.js";
+import { BadRequestError, UnAuthenticatedError } from "../../errors/index.js";
 import User from "../../models/User.js";
 import { StatusCodes } from "http-status-codes";
 
@@ -15,22 +15,35 @@ const register = async (req, res) => {
 
   const user = await User.create({ name, email, password });
   const token = user.createJWT();
-  res
-    .status(StatusCodes.CREATED)
-    .json({
-      user: {
-        email: user.email,
-        lastName: user.lastName,
-        location: user.location,
-        name: user.name,
-      },
-      token,
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
       location: user.location,
-    });
+      name: user.name,
+    },
+    token,
+    location: user.location,
+  });
 };
 
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide all values");
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    throw new UnAuthenticatedError("Invalid Credentials");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnAuthenticatedError("Invalid Credentials");
+  }
+  const token = user.createJWT();
+  user.password = undefined;
+  res.status(StatusCodes.OK).json({ user, token, location: user.location });
 };
 
 const updateUser = async (req, res) => {
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from "express";
-import "express-async-errors"
+import "express-async-errors";
+import morgan from "morgan";
 import dotenv from "dotenv";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -11,12 +12,15 @@ import connectDB from "./db/connect.js";
 
 //routers
 import authRouter from "./api/auth/auth.routes.js";
-import jobsRouter from "./api/jobs/jobs.routes.js"
+import jobsRouter from "./api/jobs/jobs.routes.js";
 // middleware
 import notFoundMiddleware from "./middleware/notFound.middleware.js";
 import errorHandlerMiddleware from "./middleware/errorHandler.middleware.js";
 
-app.use(cors())
+app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -25,7 +29,6 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", jobsRouter);
-
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
