e70fc349ff7bf6238553ddc9f46a5d561cd8a692
alexokin <alex.okin@gmail.com>
2023-06-11 09:49:20 +0300

    login functionality in place

--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -16,7 +16,7 @@ const initialState = {
 const Register = () => {
   const [values, setValues] = useState(initialState);
   const navigate = useNavigate();
-  const { user, isLoading, showAlert, displayAlert, registerUser } =
+  const { user, isLoading, showAlert, displayAlert, registerUser, loginUser } =
     useAppContext();
 
   const toggleMember = () => {
@@ -36,7 +36,7 @@ const Register = () => {
     }
     const currentUser = { name, email, password };
     if (isMember) {
-      console.log("already a member");
+      loginUser(currentUser)
     } else {
       registerUser(currentUser);
     }
