a141295ab272532309c03f50483c6c09097fb847
alexokin <alex.okin@gmail.com>
2023-06-11 18:32:37 +0300

    adding big sidebar links and css

--- client/src/components/big-sidebar.jsx
+++ client/src/components/big-sidebar.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/BigSidebar";
+import { useAppContext } from "../context/appContext";
+import NavLinks from "./nav-links";
+import Logo from "./logo";
 
 const BigSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h4>bigsidebar</h4>
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar}/>
+        </div>
+      </div>
     </Wrapper>
   );
 };
