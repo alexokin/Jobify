85acd06949a75344370ca9e2c556a6d89631c0ba
alexokin <alex.okin@gmail.com>
2023-06-11 11:41:04 +0300

    nested routes and protected route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/dashboard.jsx";
+import ProtectedRoute from "./pages/protected-route.jsx";
 import ErrorPage from "./pages/error-page.jsx";
 import LandingPage from "./pages/landing-page.jsx";
 import Register from "./pages/register.jsx";
+import {
+  AllJobs,
+  AddJob,
+  Profile,
+  SharedLayout,
+  Stats,
+} from "./pages/dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/landing" element={<LandingPage/>}/>
-        <Route path="*" element={<ErrorPage/>}/>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path="/landing" element={<LandingPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/dashboard/index.jsx
@@ -0,0 +1,7 @@
+import AddJob from "./add-job";
+import AllJobs from "./all-jobs";
+import Profile from "./profile";
+import SharedLayout from "./shared-layout";
+import Stats from "./stats";
+
+export { AllJobs, AddJob, Profile, SharedLayout, Stats };
