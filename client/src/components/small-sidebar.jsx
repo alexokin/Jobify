738f485627336323643dffbb340beae917b00434
alexokin <alex.okin@gmail.com>
2023-06-11 18:25:34 +0300

    setting up reusable navlinks component

--- client/src/components/small-sidebar.jsx
+++ client/src/components/small-sidebar.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import Logo from "./logo";
+import NavLinks from "./nav-links";
 
 const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h4>smallsidebar</h4>
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button
+            type="button"
+            className="close-btn"
+            onClick={() => toggleSidebar()}
+          >
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
     </Wrapper>
   );
 };
