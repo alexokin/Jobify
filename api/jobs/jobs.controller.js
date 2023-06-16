1b8de2866809f8992f6efcb4bf4c08cd5a26ff3b
alexokin <alex.okin@gmail.com>
2023-06-16 11:01:50 +0300

    added deleteJob to jobs controller

--- api/jobs/jobs.controller.js
+++ api/jobs/jobs.controller.js
@@ -2,6 +2,7 @@ import Job from "../../models/Job.js";
 import { StatusCodes } from "http-status-codes";
 import { BadRequestError, NotFoundError } from "../../errors/index.js";
 import checkPermissions from "../../utils/checkPermissions.js";
+import CustomAPIError from "../../errors/custom-api.js";
 
 const createJobs = async (req, res) => {
   const { position, company } = req.body;
@@ -32,19 +33,31 @@ const updateJob = async (req, res) => {
     throw new NotFoundError(`No job with id ${jobId}`);
   }
 
-  checkPermissions(req.user, job.createdBy)
+  checkPermissions(req.user, job.createdBy);
 
   const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
     new: true,
     runValidators: true,
   });
-  
-  res.status(StatusCodes.OK).json({updatedJob})
+
+  res.status(StatusCodes.OK).json({ updatedJob });
 };
 
 const deleteJob = async (req, res) => {
-  res.send("delete job");
+  const { id: jobId } = req.params;
+
+  const job = await Job.findOne({ _id: jobId });
+
+  if (!job) {
+    throw new CustomAPIError.NotFoundError(`No job with id: ${jobId}`);
+  }
+
+  checkPermissions(req.user, job.createdBy);
+
+  await job.deleteOne()
+  res.status(StatusCodes.OK).json({ msg: "Success! Job removed" });
 };
+
 const showStats = async (req, res) => {
   res.send("show stats");
 };
